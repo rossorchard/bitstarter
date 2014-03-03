e0273bb3d45df59bb5a1b941f2d7c12600d1e65b
rossorchard <rossorchard@gmail.com>
2014-03-03 10:08:44 +0000

    finished framework

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 app.use(express.logger());
+app.use('/', express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response) {
 	var out=fs.readFileSync("./index.html", "utf8");
